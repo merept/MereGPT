80843ad990133f8ac4f1b263ac85aa419a5efaa7
merept <1466005267@qq.com>
2023-06-26 13:03:26 +0800

    update

--- src/service/clearCache.py
+++ src/service/clearCache.py
@@ -2,9 +2,29 @@ import os
 
 from service.confirm import confirm
 
+units = ['Bytes', 'KB', 'MB']
+
+
+def alternate_units(num, index=0):
+    if num >= 1024 and index < 2:
+        return alternate_units(num / 1024, index + 1)
+    return f'{num:.2f} {units[index]}'
+
+
+def get_size():
+    chats_path = r'.\resource\chats'
+    chats_list = os.listdir(chats_path)
+    size = 0
+    for chat_file in chats_list:
+        file_path = os.path.join(chats_path, chat_file)
+        if os.path.isfile(file_path):
+            size += os.path.getsize(file_path)
+    return alternate_units(size)
+
 
 def clear(chat_rooms):
     os.system('cls')
+    print(f'当前占用空间 {get_size()}')
     if not confirm('是否清空记录?(Y/N)'):
         raise KeyError()
     chat_rooms.clear()
