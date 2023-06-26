b268c54df421b1b0f064ed9afbe8f84e0b2724dd
merept <1466005267@qq.com>
2023-06-26 13:31:56 +0800

    update

--- src/service/oldChat.py
+++ src/service/oldChat.py
@@ -4,6 +4,11 @@ from service.chat import chat
 from service.selectRoom import room_list
 
 
+def get_size(name):
+    path = fr'.\resource\chats\{name}.json'
+    return os.path.getsize(path)
+
+
 def old_chat(chat_rooms):
     room = room_list(chat_rooms)
     gpt = chat_rooms.gpt(room)
@@ -13,4 +18,7 @@ def old_chat(chat_rooms):
     for r in records:
         prefix = '\n\033[32mUser\033[0m' if r['role'] == 'user' else '\033[34mGPT\033[0m'
         print(f'{prefix} > {r["content"]}')
+    size = get_size(gpt.path)
+    if size > 102400:
+        print('\n\033[33mTip\033[0m > 当前聊天记录过多，建议重新开始新对话')
     chat(gpt)
