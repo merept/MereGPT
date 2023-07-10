6fef32ec5d42ef0cfc705a75ebe31698c3b521b1
merept <1466005267@qq.com>
2023-07-10 22:58:25 +0800

    update

--- src/setting/proxyUrl.py
+++ src/setting/proxyUrl.py
@@ -1,12 +1,20 @@
 import json
 import os
 
+from service.confirm import confirm
+
 
 def get_current_url(config):
     current_url = config["proxyUrl"]
     if not current_url:
         return 'https://api.openai.com/v1/chat/completions'
     return current_url
+
+
+def is_default_or_return():
+    if confirm('是否将代理地址设为默认?(Y/N)'):
+        return True
+    return False
 
 
 def set_url():
@@ -17,7 +25,8 @@ def set_url():
     print(f'当前代理地址: {get_current_url(config)}')
     new_url = input('请输入您的代理地址 > ')
     if new_url == '':
-        raise KeyboardInterrupt('apiKey')
+        if not is_default_or_return():
+            raise KeyboardInterrupt('proxyUrl')
     config['proxyUrl'] = new_url
     with open(r'.\resource\config.json', 'w', encoding='utf-8') as file:
         json.dump(config, file, ensure_ascii=False, indent=2)
