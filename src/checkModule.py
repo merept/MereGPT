391b66735796211b02baca646fdd30f0ffd6f0c0
merept <1466005267@qq.com>
2023-07-18 00:03:33 +0800

    代码文件自动修复功能

--- src/checkModule.py
+++ src/checkModule.py
@@ -4,8 +4,13 @@ import os
 
 from update import checkUpdate
 
+requestsModule = None
+files = []
+base_url = 'https://raw.githubusercontent.com/merept/MereGPT/master'
+gitee_url = 'https://gitee.com/merept/MereGPT/raw/master'
 
-def check_module(name, info):
+
+def check_third_party_module(name, info):
     package = info['package']
     version = info['version']
     try:
@@ -16,11 +21,44 @@ def check_module(name, info):
         print()
 
 
+def fix(path):
+    l_file = f'./{path}'
+    base_path = str.join('/', l_file.split('/')[:-1])
+    if not os.path.exists(base_path):
+        os.mkdir(base_path)
+    o_file = requestsModule.get(f'{base_url}/{path}')
+    with open(l_file, 'wb') as fs:
+        fs.write(o_file.content)
+
+
+def check_base_file():
+    base_files = ['src/update/update.py', 'src/update/files.json']
+    for bf in base_files:
+        if not os.path.exists(f'./{bf}'):
+            print(f'正在修复文件 {bf}')
+            fix(bf)
+
+
+def check_local_module(path):
+    if not os.path.exists(f'./{path}'):
+        print(f'正在修复文件 {path}')
+        fix(path)
+
+
 if __name__ == '__main__':
+    global requestsModule
     # os.chdir(r'..\..')
     if not os.path.exists(r'.\resource\modules.json'):
         checkUpdate.main()
     with open(r'.\resource\modules.json', 'r', encoding='utf-8') as file:
         modules_dict = json.load(file)
     for key, value in modules_dict.items():
-        check_module(key, value)
+        check_third_party_module(key, value)
+    requestsModule = importlib.import_module('requests')
+
+    check_base_file()
+
+    with open('./src/update/files.json', 'r', encoding='utf-8') as file:
+        files_list = json.load(file)
+    for f in files_list:
+        check_local_module(f)
