410c80da10768a77e71224c4fe8ed0560a334d2d
merept <1466005267@qq.com>
2023-08-20 05:34:21 +0800

    增加显示tokens数量

--- src/setting/apiKey.py
+++ src/setting/apiKey.py
@@ -15,10 +15,10 @@ def get_total_tokens():
             billing = '$0.002 / 1K tokens'
     with open('./resource/rooms.json', 'r', encoding='utf-8') as file:
         total_tokens = json.load(file)['total_tokens']
-        if total_tokens >= 10000:
-            total_tokens = f'{total_tokens / 1000:.1f}K'
         if total_tokens >= 1000000:
             total_tokens = f'{total_tokens / 100000:.1f}M'
+        elif 1000000 > total_tokens >= 10000:
+            total_tokens = f'{total_tokens / 1000:.1f}K'
     return billing, total_tokens
 
 
